1e6c8c966e92dad68f6bf9e61d28dacac9162941
Jon <lundbyjon@hotmail.com>
2023-04-17 15:22:00 +0200

    fetch json from firebase implemented

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 
 window.addEventListener("load", startApp);
 
-function startApp() {
+const endpoint = "https://test01-6591a-default-rtdb.europe-west1.firebasedatabase.app";
+const arrayExample = [2, 6, 2, 1];
+
+async function startApp() {
   console.log("window loaded and startApp called...");
+
+  const allPosts = await getPosts();
+
+  console.log(allPosts);
+  console.log(arrayExample);
+}
+
+async function getPosts() {
+  const response = await fetch(`${endpoint}/posts.json`);
+  const data = await response.json();
+
+  return data;
 }
