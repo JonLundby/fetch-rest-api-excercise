c57f678f443c8dedcffe1439ad0d025c1386b7ea
Jon <lundbyjon@hotmail.com>
2023-04-18 11:52:10 +0200

    user funtions added copy pasted from posts. createUser added

--- script.js
+++ script.js
@@ -7,13 +7,22 @@ const endpoint = "https://test01-6591a-default-rtdb.europe-west1.firebasedatabas
 
 async function startApp() {
   const allPosts = await getPosts();
+  const allusers = await getUsers();
 
   console.log(allPosts);
 
   for (const element of allPosts) {
     displayPosts(element);
   }
+
+  for (const element of allusers) {
+    displayUsers(element);
+  }
+
+  createPost("some title", null, "some body text...");
 }
+
+//    ---- POST FUNCTIONS ----    \\
 
 async function getPosts() {
   const response = await fetch(`${endpoint}/posts.json`);
@@ -44,6 +53,63 @@ function displayPosts(allPosts) {
                       <div id="post-image"><img src="${allPosts.image}"></div>
                       <p id="post-body">${allPosts.body}</p>
                     </div>`;
-  
+
   document.querySelector("#posts-container").insertAdjacentHTML("beforeend", postsGridItem);
+}
+
+async function createPost(title, image, body) {
+  const newPost = {
+    title: title,
+    image: image,
+    body: body,
+  };
+
+  const newPostAsJson = JSON.stringify(newPost);
+  //console.log(newPostAsJson);
+
+  const response = await fetch(`${endpoint}/posts.json`, {
+    method: "POST",
+    body: newPostAsJson,
+  });
+  //console.log(response);
+
+  const data = response.json();
+  console.log(data);
+}
+
+//    ---- USER FUNCTIONS ----    \\
+
+async function getUsers() {
+  const response = await fetch(`${endpoint}/users.json`);
+  const data = await response.json();
+  console.log(data);
+
+  const allUsersArray = prepareUserData(data);
+
+  return allUsersArray;
+}
+
+function prepareUserData(dataObject) {
+  const userArray = [];
+
+  for (const key in dataObject) {
+    const user = dataObject[key];
+    user.id = key;
+    userArray.push(user);
+  }
+
+  return userArray;
+}
+
+function displayUsers(allUsers) {
+  const usersGridItem = /*html*/ `
+                    <div id="user">
+                      <div id="user-image"><img src="${allUsers.image}"></div>
+                      <p id="post-title">${allUsers.name}</p>
+                      <p id="user-body">${allUsers.title}</p>
+                      <p id="user-body">${allUsers.phone}</p>
+                      <p id="user-body">${allUsers.mail}</p>
+                    </div>`;
+
+  document.querySelector("#users-container").insertAdjacentHTML("beforeend", usersGridItem);
 }
