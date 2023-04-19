454256bc8320b797d74289b9f7ce94dff8d5615d
Jon <lundbyjon@hotmail.com>
2023-04-19 11:26:21 +0200

    delete post working and update post sketch

--- script.js
+++ script.js
@@ -6,18 +6,20 @@ const endpoint = "https://test01-6591a-default-rtdb.europe-west1.firebasedatabas
 //const arrayExample = [2, 6, 2, 1];
 
 async function startApp() {
-  const allPosts = await getPosts();
-  const allusers = await getUsers();
+  // const allPosts = await getPosts();
+  // const allusers = await getUsers();
 
-  console.log(allPosts);
+  // console.log(allPosts);
 
-  for (const element of allPosts) {
-    displayPosts(element);
-  }
+  // for (const element of allPosts) {
+  //   displayPost(element);
+  // }
 
-  for (const element of allusers) {
-    displayUsers(element);
-  }
+  // for (const element of allusers) {
+  //   displayUsers(element);
+  // }
+
+  updatePostsGrid();
 
   //createPost("some title", null, "some body text...");
 }
@@ -46,16 +48,60 @@ function preparePostData(dataObject) {
   return postArray;
 }
 
-function displayPosts(allPosts) {
+function displayPost(post) {
   const postsGridItem = /*html*/ `
                     <div id="post">
-                      <p id="post-title">${allPosts.title}</p>
-                      <div id="post-image"><img src="${allPosts.image}"></div>
-                      <p id="post-body">${allPosts.body}</p>
+                      <p id="post-title">${post.title}</p>
+                      <div id="post-image"><img src="${post.image}"></div>
+                      <p id="post-body">${post.body}</p>
+                      <span id="buttons">
+                        <button class="update">UPDATE</button>
+                        <button class="delete">DELETE</button>
+                      </span>
+                      
                     </div>`;
 
   document.querySelector("#posts-container").insertAdjacentHTML("beforeend", postsGridItem);
+
+  document.querySelector("#post:last-child .update").addEventListener("click", updatePostClicked);
+  document.querySelector("#post:last-child .delete").addEventListener("click", deletePostClicked);
+
+  function updatePostClicked() {
+    console.log("post update btn clicked...");
+    updatePost();
+  }
+
+  function updatePost() {
+    console.log("updating post...");
+  }
+
+  function deletePostClicked() {
+    deletePost(post.id);
+  }
+
+  async function deletePost(id) {
+    const response = await fetch(`${endpoint}/posts/${id}.json`, { method: "DELETE" });
+    if (response.ok) {
+      console.log("post deleted!")
+      updatePostsGrid();
+    }
+  }
+
 }
+
+async function updatePostsGrid() {
+  const posts = await getPosts();
+  //displayPost(posts);
+  displayPosts(posts);
+}
+
+function displayPosts(posts) {
+  document.querySelector("#posts-container").innerHTML = "";
+  for (const element of posts) {
+    displayPost(element);
+  }
+}
+
 
 async function createPost(title, image, body) {
   const newPost = {
@@ -112,4 +158,10 @@ function displayUsers(allUsers) {
                     </div>`;
 
   document.querySelector("#users-container").insertAdjacentHTML("beforeend", usersGridItem);
+
+  document.querySelector("#user:last-child").addEventListener("click", userDetailView);
+}
+
+function userDetailView() {
+  console.log("user clicked for detailed view...");
 }
